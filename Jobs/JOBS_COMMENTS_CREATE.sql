25b9ede3e95d6afd0c4d0a07239d08af4797e391
Cooper Click <ccubed.techno@gmail.com>
2015-09-11 23:26:50 -0400

    Finished testing per comment and per job reader flags.

--- Jobs/JOBS_COMMENTS_CREATE.sql
+++ Jobs/JOBS_COMMENTS_CREATE.sql
@@ -4,7 +4,7 @@ CREATE TABLE `[INSERT YOUR DB HERE]`.`JOBS_COMMENTS`(
   `POSTED_BY` INT NOT NULL COMMENT 'Holds DBREF of poster minus #',
   `POSTED_ON` DATETIME COMMENT 'Holds datetime of post',
   `FLAGS` INT(1) NOT NULL DEFAULT 0 COMMENT 'Flag indicator for post',
-  `READERS` TINYTEXT NULL DEFAULT NULL COMMENT 'Holds list of reader DBREFs without #',
+  `READERS` TINYTEXT NULL COMMENT 'Holds list of reader DBREFs without #',
   `BODY` LONGTEXT NOT NULL COMMENT 'Stores comment text',
   PRIMARY KEY (`ID`),
   FULLTEXT (BODY),
